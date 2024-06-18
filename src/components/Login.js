d57ce7c0708dcea8c33b4dcb4551e81c49729531
Harshit Vats <hvats16@gmail.com>
2024-06-18 07:17:56 +0530

    SignIn and SignUp form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -13,9 +19,18 @@ const Login = () => {
         />
       </div>
       <form className="p-12 bg-[rgba(0,0,0,0.8)] absolute w-3/12 my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full rounded-sm bg-[rgba(22,22,22,0.7)] border border-[rgba(255,255,255,0.5)]"
+          />
+        )}
         <input
-          type="text"
+          type="email"
           placeholder="Email Address"
           className="p-4 my-4 w-full rounded-sm bg-[rgba(22,22,22,0.7)] border border-[rgba(255,255,255,0.5)]"
         />
@@ -25,8 +40,17 @@ const Login = () => {
           className="p-4 my-4 w-full rounded-sm bg-[rgba(22,22,22,0.7)] border border-[rgba(255,255,255,0.5)]"
         />
         <button className="p-3 my-3 bg-red-700 w-full rounded-sm font-bold">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p
+          className="py-4"
+          onClick={toggleSignInForm}
+          style={{ cursor: "pointer" }}
+        >
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already on Netflix? Sign In Now"}
+        </p>
       </form>
     </div>
   );
